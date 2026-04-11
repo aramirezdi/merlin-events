c4b9c0c7f7740bd5ce2b4f1ae138324e68591573
aramirezdi <a_ramirezdi@yahoo.es>
2026-04-10 23:31:26 -0500

    Add hub landing page, rename index to asistencia, serve hub at root

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'hub.html')));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => console.log(`Merlin Events running on port ${PORT}`));
